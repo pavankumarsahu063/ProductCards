bdc0ef49f1b70222a023e46b62070d63c3472189
pavankumar063 <pavankumar987321@gmail.com>
2025-04-02 23:49:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const div=document.getElementById("main");
+
+
+const api="https://fakestoreapi.com/products";
+
+
+document.addEventListener("DOMContentLoaded",async ()=>{
+    let res=await fetch(api);
+    let data=await res.json();
+    console.log(data);
+
+    let currPage=data.slice(0,4);
+    console.log(currPage);
+
+    
+    
+    currPage.forEach(element => {
+        
+        console.log(element);
+        
+       
+        
+
+        
+        let card=document.createElement("div");
+        card.className="cards";
+        card.innerHTML=`
+        <h1 class='title'>${element.title}</h1>`
+        div.appendChild(card);
+
+        
+        
+    });
+    let prevBtn=document.createElement("button").innerText="prev";
+    prevBtn.className="btn";
+    
+    document.querySelector(".btn").style.margin="20px";
+    let nextBtn=document.createElement("button").innerText="Next";
+
+    div.append(prevBtn,nextBtn);
+    
+})
+
